ba82f4cdbeadf3ea3be634bda7e79a30880fccd2
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-09-07 15:28:44 +0300

    9.1. String [#237407 #199999]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -3,7 +3,10 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie "; //конкатенация
         int year = 2021;
-        System.out.println(idea + " But I am a newbie " + year);
+        idea += year; //конкатенация
+        System.out.println(idea);
     }
 }
