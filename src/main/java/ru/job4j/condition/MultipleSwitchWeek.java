91d1be732d299d3d01c57a5992b976d7ae94ef2d
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-09-15 08:16:02 +0300

    4.6.2. Switch - matcher [#334071]

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -7,16 +7,12 @@ public class MultipleSwitchWeek {
             case "Вторник", "Tuesday" -> 2;
             case "Среда", "Wednesday" -> 3;
             case "Четверг", "Thursday" -> 4;
-            case "пятница", "Пятница", "Friday" -> 5;
+            case "Пятница", "Friday" -> 5;
             case "Суббота", "Saturday" -> 6;
             case "Воскресенье", "Sunday" -> 7;
             default -> -1;
         };
     }
 
-    public static void main(String[] args) {
-        int rsl = MultipleSwitchWeek.numberOfDay("пятница");
-        System.out.println(rsl);
-    }
 }
 
