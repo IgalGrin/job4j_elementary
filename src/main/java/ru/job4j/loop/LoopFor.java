59c31f83e36f831f3ddd8059ed423db2cc307da7
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-09-22 09:47:23 +0300

    5.2. Создать программу, вычисляющую факториал. [#193]

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -1,9 +1,9 @@
 package ru.job4j.loop;
 
-public class LoopFor { @SuppressWarnings("checkstyle:InnerAssignment")
+public class LoopFor {
 public static void main(String[] args) {
     int i;
-    for (i = 5; i <= 10; i++) {
+    for (i = 0; i <= 5; i++) {
         System.out.println(i);
     }
     System.out.println("Индекс после цикла равен: " + i);
