0edd7f78a2cc4651cebeb44511e1423c76ca8b90
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-09-24 18:38:05 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        double debt = amount + amount * percent / 100;
+        while (debt > 0) {
+            debt = debt - salary;
+            year += 1;
+        }
+
+        return year;
+    }
+}
